a9b8dcf83b2a70f1b8fd45be474374a78aaf5279
devmgs92 <devmgs92@gmail.com>
2019-05-18 00:54:13 -0700

    Penambahan menu dan Form Detail Pemesanan

---
+++ app/src/main/java/com/mgs/pesantukang/activity/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.mgs.pesantukang.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mgs.pesantukang.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
