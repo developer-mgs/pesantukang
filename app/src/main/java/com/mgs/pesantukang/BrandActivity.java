b474ea277b628e786825a3c7a31edac627a2f9af
devmgs92 <devmgs92@gmail.com>
2019-05-23 20:40:20 -0700

    Penambahan fitur zoom image Detail Activity

--- app/src/main/java/com/mgs/pesantukang/BrandActivity.java
+++ app/src/main/java/com/mgs/pesantukang/BrandActivity.java
@@ -1,7 +1,19 @@
 package com.mgs.pesantukang;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.gitonway.lee.niftymodaldialogeffects.lib.Effectstype;
+import com.gitonway.lee.niftymodaldialogeffects.lib.NiftyDialogBuilder;
 
 public class BrandActivity extends AppCompatActivity {
 
@@ -11,4 +23,39 @@ public class BrandActivity extends AppCompatActivity {
         setContentView(R.layout.activity_brand);
         getSupportActionBar().setTitle("Official Brand");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.about_brand,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.openAbout:
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        this);
+                alertDialogBuilder.setTitle("Official Brand");
+                alertDialogBuilder
+                        .setMessage("Official Brand adalah halaman resmi brand material di aplikasi Pesantukang dimana kustomer dapat memesan" +
+                                "tukang yang sudah ditunjuk dan dipercaya oleh Brand.")
+                        //.setIcon(R.mipmap.ic_launcher)
+                        .setCancelable(false)
+                        .setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+                return true;
+
+        }
+        return false;
+    }
+
 }
--- app/src/main/java/com/mgs/pesantukang/activity/DetailActivity.java
+++ app/src/main/java/com/mgs/pesantukang/activity/DetailActivity.java
@@ -2,12 +2,19 @@ package com.mgs.pesantukang.activity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.github.chrisbanes.photoview.PhotoView;
 import com.gitonway.lee.niftymodaldialogeffects.lib.Effectstype;
 import com.gitonway.lee.niftymodaldialogeffects.lib.NiftyDialogBuilder;
 import com.mgs.pesantukang.R;
@@ -15,14 +22,34 @@ import com.mgs.pesantukang.R;
 public class DetailActivity extends AppCompatActivity {
     NiftyDialogBuilder dialogs;
     Button btn_pilih1,btn_pilih2;
+    ImageView mIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         getSupportActionBar().setTitle("AC");
         dialogs = NiftyDialogBuilder.getInstance(this);
+        mIcon = findViewById(R.id.imgDetail);
         btn_pilih1 = (Button) findViewById(R.id.btnPilih1);
         btn_pilih2 = (Button) findViewById(R.id.btnPilih2);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.gb_1);
+        RoundedBitmapDrawable mDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        mDrawable.setCircular(false);
+        mIcon.setImageDrawable(mDrawable);
+
+        mIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(DetailActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_custom_layout, null);
+                PhotoView photoView = mView.findViewById(R.id.imageViewDetail);
+                photoView.setImageResource(R.drawable.gb_1);
+                mBuilder.setView(mView);
+                AlertDialog mDialog = mBuilder.create();
+                mDialog.show();
+            }
+        });
 
         btn_pilih1.setOnClickListener(new View.OnClickListener() {
             @Override
